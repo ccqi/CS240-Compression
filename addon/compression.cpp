0611be6dfd50c14ee922f8aafa22a4ab01a332b8
Charles Qi <cqi@oanda.com>
2015-10-11 00:22:58 -0400

    Removed comments

--- addon/compression.cpp
+++ addon/compression.cpp
@@ -5,29 +5,6 @@
 using namespace std;
 using namespace v8;
 
-/*void Encode (const Nan::FunctionCallbackInfo<v8::Value>& args) {
-  
-  v8::String::Utf8Value param1(args[0]->ToString());
-  std::string plainText = std::string(*param1);
-  
-  TextComponent * text;
-  BITS whole_data;
-  Encoding * encoding;
-  encoding = new Encoding(plainText, TEXT);
-  text = new PlainText(encoding);
-  text = new BWTransform(text);
-  Encoding * curEncoding = text->encode();
-  string cipherText = Encoding::convertToString(curEncoding->getBits());
-
-  v8::Local<v8::String> jsText = Nan::New(cipherText).ToLocalChecked();
-  args.GetReturnValue().Set(jsText);
-}*/
-
-/*void Init(v8::Local<v8::Object> exports) {
-  exports->Set(Nan::New("encode").ToLocalChecked(),
-               Nan::New<v8::FunctionTemplate>(Encode)->GetFunction());
-}*/
-
 void Init(v8::Local<v8::Object> exports) {
   TextWrapper::Init(exports);
 }
